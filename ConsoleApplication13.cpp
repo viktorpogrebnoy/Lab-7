efddbd54b3bbc63bbfc32a2e20597c9d8a2de95e
viktorpogrebnoy <118654747+viktorpogrebnoy@users.noreply.github.com>
2022-11-20 10:26:17 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	float y, Z, Dob;
+	cout << "Enter a = ";
+	cin >> y;
+	Dob = 1;
+	for (int i = 1; i < 11; i++)
+	{
+		Dob *= log(i * y);
+	}
+	Z = cos(Dob) / y + 2;
+	cout << "Z=" << Z << endl;
+}
